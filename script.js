a963fc231ab63e5704f4c7d4b8205fda9b237ad8
Hermann <188955756+4483-s@users.noreply.github.com>
2025-05-21 17:36:03 +0800

    Add check winner logic

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const gameboard = (function () {
   const checkResult = () => {
     for (const r of rows) {
       if (r.every((v) => v === r[0] && v)) {
-        return `${r[0]} wins`;
+        return r[0];
       }
     }
     for (let i = 0; i < 3; i++) {
@@ -23,9 +23,19 @@ const gameboard = (function () {
         rows[0][i] === rows[2][i] &&
         rows[0][i]
       ) {
-        return `${r[0][i]} wins`;
+        return r[0][i];
       }
     }
+    if (rows[0][0] === rows[1][1] && rows[0][0] === rows[2][2] && rows[0][0]) {
+      return rows[0][0];
+    } else if (
+      rows[0][2] === rows[1][1] &&
+      rows[0][2] === rows[2][0] &&
+      rows[0][2]
+    ) {
+      return rows[0][0];
+    } else if (rows.flat().every((v) => v)) return "tie";
+    else return "ongoing";
   };
   const takeInput = (input, row, column) => {
     rows[row - 1][column - 1] = input ? "O" : "X";
