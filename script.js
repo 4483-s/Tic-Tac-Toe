9e623c1d6fc29a79e286f8d57383b3202fe42f2a
Julius <188955756+4483-s@users.noreply.github.com>
2025-05-25 18:21:23 +0800

    Header change textContent

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const makePlayer = function (name) {
   return { name, placeMark };
 };
 const dom = (function () {
+  const oRadio = document.querySelector("#o");
+  const bar = document.querySelector(".bar");
   const playBtn = document.querySelector(".play");
-  const resetBtn = document.querySelector(".reset");
+  const resetBtn = document.createElement("button");
   const dialog = document.querySelector("dialog");
   const cancel = document.querySelector(".cancel");
   const rows = document.querySelectorAll(".rows");
@@ -13,15 +15,39 @@ const dom = (function () {
   const h1 = document.querySelector("h1");
   const oplayer = makePlayer("O");
   const xplayer = makePlayer("X");
+  resetBtn.classList.add("reset");
+  resetBtn.textContent = "Reset";
   confirm.addEventListener("click", () => {
+    if (oRadio.checked) {
+      gameboard.currentMark = true;
+    } else {
+      gameboard.currentMark = false;
+    }
+    bar.append(resetBtn);
+    playBtn.remove();
     oplayer.name = document.querySelector("#oplayer").value;
     xplayer.name = document.querySelector("#xplayer").value;
+    const showTurn = function () {
+      if (!gameboard.isOngoing) {
+        return;
+      }
+      if (gameboard.currentMark) {
+        h1.textContent = `${oplayer.name}'s (O) turn`;
+      } else {
+        h1.textContent = `${xplayer.name}'s (X) turn`;
+      }
+    };
+    showTurn();
     resetBtn.addEventListener("click", () => {
+      h1.style.color = "#ffffff";
       gameboard.clearBoard();
+      gameboard.isOngoing = true;
+      showTurn();
+
       document.querySelectorAll(".rows > div").forEach((v) => {
+        v.style.backgroundColor = "#1b1b1b";
         v.textContent = "";
       });
-      return;
     });
     dialog.close();
 
@@ -29,6 +55,7 @@ const dom = (function () {
       row.querySelectorAll("div").forEach((cell, i) =>
         cell.addEventListener("click", () => {
           cellClicked(cell, index, i, oplayer, xplayer, h1);
+          showTurn();
         })
       );
     });
@@ -37,39 +64,45 @@ const dom = (function () {
     dialog.show();
   });
   cancel.addEventListener("click", () => dialog.close());
-  return { confirm };
+  return { rows };
 })();
 
 //
 function cellClicked(cell, index, i, oplayer, xplayer, h1) {
-  if (gameboard.getBoard()[index][i]) {
+  if (gameboard.getBoard()[index][i] || !gameboard.isOngoing) {
     return;
   }
   if (gameboard.currentMark) {
     oplayer.placeMark(true, index, i);
     cell.textContent = "O";
+    cell.style.color = "#75c4f8";
   } else {
     xplayer.placeMark(false, index, i);
 
     cell.textContent = "X";
+    cell.style.color = "#fcde5a";
   }
   if (gameboard.checkGameStatus() === "O") {
     h1.textContent = `${oplayer.name} wins`;
-    h1.style.color = "#b04bb9";
+    h1.style.color = "#01f801";
+    gameboard.isOngoing = false;
   }
   if (gameboard.checkGameStatus() === "X") {
     h1.textContent = `${xplayer.name} wins`;
-    h1.style.color = "#b04bb9";
+    h1.style.color = "#01f801";
+    gameboard.isOngoing = false;
   }
   if (gameboard.checkGameStatus() === "tie") {
     h1.textContent = "Tie";
     h1.style.color = "#b04bb9";
+    gameboard.isOngoing = false;
   } else {
     gameboard.currentMark = !gameboard.currentMark;
   }
 }
 
 const gameboard = (function () {
+  const isOngoing = true;
   const rows = [
     ["", "", ""],
     ["", "", ""],
@@ -83,27 +116,45 @@ const gameboard = (function () {
   };
   const getBoard = () => rows;
   const checkGameStatus = () => {
-    for (const r of rows) {
-      if (r.every((v) => v === r[0] && v)) {
-        return r[0];
+    for (let i = 0; i < 3; i++) {
+      if (rows[i].every((v) => v === rows[i][0] && v)) {
+        dom.rows[i]
+          .querySelectorAll("div")
+          .forEach(
+            (v) => (v.style.backgroundColor = "rgba(129, 245, 129, 0.4)")
+          );
+        return rows[i][0];
       }
     }
+
     for (let i = 0; i < 3; i++) {
       if (
         rows[0][i] === rows[1][i] &&
         rows[0][i] === rows[2][i] &&
         rows[0][i]
       ) {
+        for (const row of dom.rows) {
+          row.querySelectorAll("div")[i].style.backgroundColor =
+            "rgba(129, 245, 129, 0.4)";
+        }
         return rows[0][i];
       }
     }
     if (rows[0][0] === rows[1][1] && rows[0][0] === rows[2][2] && rows[0][0]) {
+      for (let i = 0; i < 3; i++) {
+        dom.rows[i].querySelectorAll("div")[i].style.backgroundColor =
+          "rgba(129, 245, 129, 0.4)";
+      }
       return rows[0][0];
     } else if (
       rows[0][2] === rows[1][1] &&
       rows[0][2] === rows[2][0] &&
       rows[0][2]
     ) {
+      for (let i = 0, j = 2; i < 3; i++, j--) {
+        dom.rows[i].querySelectorAll("div")[j].style.backgroundColor =
+          "rgba(129, 245, 129, 0.4)";
+      }
       return rows[0][0];
     } else if (rows.flat().every((v) => v)) return "tie";
     else return "ongoing";
@@ -112,5 +163,12 @@ const gameboard = (function () {
     rows[row][column] = input ? "O" : "X";
   };
   const currentMark = true;
-  return { clearBoard, getBoard, checkGameStatus, takeInput, currentMark };
+  return {
+    clearBoard,
+    getBoard,
+    checkGameStatus,
+    takeInput,
+    currentMark,
+    isOngoing,
+  };
 })();
